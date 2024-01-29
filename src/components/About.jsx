63fffaa8d721058f44a9a81cd138cd29804e8e42
Ai-riku <116927374+Ai-riku@users.noreply.github.com>
2024-01-28 18:12:06 -0800

    Added About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
-const About = () => {
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5*index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title}
+          className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div>
+        <p className={styles.heroSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p 
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4'
+      >
+        I'm an experienced software engineer with a background in the Fintech industry. Skilled in C++, Python, SQL, Machine Learning, and web development technologies, with a focus on new feature implementation, testing, and documentation. Proven ability to collaborate with global teams and adhere to tight deadlines for business-critical deliverables.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
+// export default About
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -2.25, -1.5]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -1.5, -1.5] : [0, -2.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
