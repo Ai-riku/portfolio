a1c76212f993f53b791f9679a83b057db0e4a7ef
Ai-riku <116927374+Ai-riku@users.noreply.github.com>
2024-02-07 17:31:47 -0800

    updated computer 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      {/* <RetroComputerCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
